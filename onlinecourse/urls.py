de4c8c7b08eaf8e7873df7cbcdfff37a5e5cda88
enpie <155533024+enpie@users.noreply.github.com>
2026-07-08 04:56:54 +0700

    Update urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'onlinecourse'
 urlpatterns = [
     path('<int:course_id>/submit/', views.submit, name='submit'),
-    path('<int:course_id>/show_exam_result/', views.show_exam_result, name='show_exam_result'),
+    path('<int:course_id>/submission/<int:submission_id>/show_exam_result/', views.show_exam_result, name='show_exam_result'),
 ]
