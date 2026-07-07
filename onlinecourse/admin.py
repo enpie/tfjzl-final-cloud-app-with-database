5b86afcfebe23121233b31da4b19cd8c33b58e2a
enpie <155533024+enpie@users.noreply.github.com>
2026-07-08 04:52:06 +0700

    Create admin.py

---
+++ onlinecourse/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Course, Lesson, Question, Choice, Submission, Enrollment
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline, QuestionInline]
+    list_display = ('name', 'pub_date')
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'course', 'grade']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course']
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
+admin.site.register(Enrollment)
